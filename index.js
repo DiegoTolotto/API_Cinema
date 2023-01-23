00ea6e853416842d084c6556a3530a61cc8cb25b
Diego Tolotto <diego.tolotto8@gmail>
2023-01-23 10:47:37 -0300

    Conectando com  MongoDB Atlas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //config incial
+const { application } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express()
@@ -13,27 +14,30 @@ app.use(
 )
 
 // rotas da API
-app.post('/', async (req, res) => {
+const personRouters = require('./routes/clienteRoutes')
 
-    //req.body
-    const { nome, idade, maiorDeIdade, estudante } = req.body
+app.use()
 
-    const cliente = {
-        nome, 
-        idade,
-        maiorDeIdade,
-        estudante
-    }
-
-    try {
-        
-        
-
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
-})
 
 // rotaincial / endpoint
+app.get('/', (req, res) => {
+
+    //mostra req
+
+    res.json({ message: 'Oi Express!' })
+})
+
 
 // entragar porta
+const DB_USER = ''
+const DB_PASSWORD = ''
+
+    // Por segurança naõ vou subir ao Github o meu User e nem minha senha 
+
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.kz5bzvm.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Conectamos com sucesso')
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -4,6 +4,26 @@ const Cliente = require('../models/Cliente')
 
 
  // Create
+ router.post('/', async (req, res) => {
+
+    //req.body
+    const { nome, idade, maiorDeIdade, estudante } = req.body
+
+    const cliente = {
+        nome, 
+        idade,
+        maiorDeIdade,
+        estudante
+    }
+
+    try {
+        
+        
+
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
 
  // Read
 
