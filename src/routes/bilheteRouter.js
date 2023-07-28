8734d25ba40dffb242bcc9710866c9cfaee50d53
Diego Tolotto <diego.tolotto8@gmail>
2023-07-27 22:05:51 -0300

    Criando esboço da rota Bilhete

---
+++ src/routes/bilheteRouter.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const Bilhete = require('../models/Bilhete')
+
+
+router.post('/', async (req, res) => {
+
+
+});
+
+
+router.get('/', async (req, res) => {
+
+
+    
+});
+
+router.get('/:id', async (req, res) => {
+
+
+})
+
+
+router.patch('/:id', async (req, res) => {
+
+})
+
+
+router.delete('/:id', async (req, res) => {
+
+})
+
+module.exports = router
