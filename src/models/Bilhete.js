a2d0b8307b5da4280f7a0f01aa5d220ef7c1525e
Diego Tolotto <diego.tolotto8@gmail>
2023-07-27 21:28:40 -0300

    A rota bilhete terá apenas um atributo

--- src/models/Bilhete.js
+++ src/models/Bilhete.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bilheteShcema = new Schema({
-
+    assentos: Number
 })
 
 const Bilhete = mongoose.model('bilhete', bilheteShcema)
