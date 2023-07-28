4037a86b208c0c009e0d08274bf1b6f3663e3da4
Diego Tolotto <diego.tolotto8@gmail>
2023-07-27 22:11:25 -0300

    Adicionado requisição de pots

--- src/routes/bilheteRouter.js
+++ src/routes/bilheteRouter.js
@@ -4,18 +4,29 @@ const Bilhete = require('../models/Bilhete')
 
 router.post('/', async (req, res) => {
 
+    const { assentos } = req.body
 
+    const salas = {
+        assentos
+    }
+
+    try {
+        
+        await Bilhete.create(salas)
+
+        res.status(200).json({ message: "Bilhete criado com sucesso"})
+
+    } catch (error) {
+        res.status(500).json({ error: error})
+    }
 });
 
 
 router.get('/', async (req, res) => {
 
-
-    
 });
 
 router.get('/:id', async (req, res) => {
-
 
 })
 
