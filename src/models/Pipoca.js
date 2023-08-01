5ce0377872a8a0a5fdde67ff7f61766fe4d70f54
Diego Tolotto <diego.tolotto8@gmail>
2023-07-31 22:01:28 -0300

    Começando nova rota com o model

---
+++ src/models/Pipoca.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const pipocaShcema = new Schema({
+   
+})
+
+const Pipoca = mongoose.model('pipoca', pipocaShcema)
+
+module.exports = Pipoca;
