fee698fa2f4d9d3d264afc4da4ce54ec1c8513c2
Diego Tolotto <diego.tolotto8@gmail>
2023-07-31 22:04:07 -0300

    Adicionado atributos

--- src/models/Pipoca.js
+++ src/models/Pipoca.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const pipocaShcema = new Schema({
-   
+   produto: String,
+   price: Number
 })
 
 const Pipoca = mongoose.model('pipoca', pipocaShcema)
