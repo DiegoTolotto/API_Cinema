cd0ed18a3c5b1bd4bba4c1a81c5d51518fafca53
Diego Tolotto <diego.tolotto8@gmail>
2023-10-06 17:25:28 -0300

    Criando controlador de usuario

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,3 +10,6 @@ exports.allAccess = (req, res) => {
     res.status(200).send("Admin Content.");
   };
   
+  exports.moderatorBoard = (req, res) => {
+    res.status(200).send("Moderator Content.");
+  }
