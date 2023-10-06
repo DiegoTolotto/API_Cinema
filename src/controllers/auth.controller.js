1f82b4cf2f8ea324804cd5e5f3d69f5603caf9ad
Diego Tolotto <diego.tolotto8@gmail>
2023-10-06 17:23:57 -0300

    Criando controlador de usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -12,4 +12,111 @@ exports.signup = (req, res) => {
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password, 8),
     });
-}
+
+    user.save((err, user) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        if (req.body.roles) {
+            Role.find(
+              {
+                name: { $in: req.body.roles },
+              },
+              (err, roles) => {
+                if (err) {
+                  res.status(500).send({ message: err });
+                  return;
+                }
+      
+                user.roles = roles.map((role) => role._id);
+                user.save((err) => {
+                  if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                  }
+      
+                  res.send({ message: "Usuario cadastrado com sucesso!" });
+                });
+              }
+            );
+          } else {
+            Role.findOne({ name: "user" }, (err, role) => {
+              if (err) {
+                res.status(500).send({ message: err });
+                return;
+              }
+      
+              user.roles = [role._id];
+              user.save((err) => {
+                if (err) {
+                  res.status(500).send({ message: err });
+                  return;
+                }
+      
+                res.send({ message: "Usuario cadastrado com sucesso!" });
+              });
+            });
+          }
+        });
+};
+
+
+exports.signin = (req, res) => {
+    User.findOne({
+      username: req.body.username,
+    })
+      .populate("roles", "-__v")
+      .exec((err, user) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+  
+        if (!user) {
+          return res.status(404).send({ message: "Usuario não encontrado." });
+        }
+  
+        var passwordIsValid = bcrypt.compareSync(
+          req.body.password,
+          user.password
+        );
+  
+        if (!passwordIsValid) {
+          return res.status(401).send({ message: "Senha invalida!" });
+        }
+  
+        const token = jwt.sign({ id: user.id },
+                                config.secret,
+                                {
+                                  algorithm: 'HS256',
+                                  allowInsecureKeySizes: true,
+                                  expiresIn: 86400, // 24 hours
+                                });
+  
+        var authorities = [];
+  
+        for (let i = 0; i < user.roles.length; i++) {
+          authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+        }
+  
+        req.session.token = token;
+  
+        res.status(200).send({
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          roles: authorities,
+        });
+      });
+  };
+  
+  exports.signout = async (req, res) => {
+    try {
+      req.session = null;
+      return res.status(200).send({ message: "Você foi desconectado!" });
+    } catch (err) {
+      this.next(err);
+    }
+  };
